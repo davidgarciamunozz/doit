684b65402aa08b9ea6ac1051cc1eae8705ff5a4f
Sara Maya <saraisabelmayalasso@gmail.com>
2025-09-28 15:49:58 -0500

    feat(orders): Create Orders-Form page

---
+++ src/app/orders/form/page.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/components/layout";
+
+export default function OrdersPage() {
+  return (
+    <Layout>
+      <div>
+        <h1>form</h1>
+      </div>
+    </Layout>
+  );
+}

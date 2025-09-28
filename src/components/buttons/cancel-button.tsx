f31ce5b7895a47d752e81507ca6622e5de666ff0
Sara Maya <saraisabelmayalasso@gmail.com>
2025-09-28 15:44:56 -0500

    feat(botones): agregar SaveButton y CancelButton

---
+++ src/components/buttons/cancel-button.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { Button } from "@/components/ui/button";
+
+type CancelButtonProps = {
+  onClick?: () => void;
+};
+
+export default function CancelButton({ onClick }: CancelButtonProps) {
+  return (
+    <Button
+      onClick={onClick}
+      className="rounded-lg bg-gray-800 text-white font-semibold px-8 py-3 hover:bg-gray-700"
+    >
+      Cancel
+    </Button>
+  );
+}

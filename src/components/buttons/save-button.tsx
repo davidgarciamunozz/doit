0bee62232e5ff05ba3decf6332a414e0994b7a50
julianalinares88 <julianalinares1701@gmail.com>
2025-09-28 22:49:00 -0500

    feat(ui): agregar nuevas propiedades al saveButton

--- src/components/buttons/save-button.tsx
+++ src/components/buttons/save-button.tsx
@@ -3,15 +3,20 @@ import { Button } from "@/components/ui/button";
 
 type SaveButtonProps = {
   onClick?: () => void;
+  label?: string;
 };
 
-export default function SaveButton({ onClick }: SaveButtonProps) {
+export default function SaveButton({
+  onClick,
+  label = "Save",
+}: SaveButtonProps) {
   return (
     <Button
+      type="submit" // importante para que dispare el submit del form
       onClick={onClick}
-      className="rounded-lg bg-lime-300 text-black font-semibold px-8 py-3 hover:bg-lime-400"
+      className="rounded-lg bg-lime-300 text-black font-semibold px-8 py-3 hover:bg-lime-400 w-full"
     >
-      Save
+      {label}
     </Button>
   );
 }

3ec926be4944a919e168c8e7227d458190401c48
Sara Maya <saraisabelmayalasso@gmail.com>
2025-09-29 07:49:48 -0500

    style(cancel-button): update background color and hover

--- src/components/buttons/cancel-button.tsx
+++ src/components/buttons/cancel-button.tsx
@@ -9,7 +9,7 @@ export default function CancelButton({ onClick }: CancelButtonProps) {
   return (
     <Button
       onClick={onClick}
-      className="rounded-lg bg-gray-800 text-white font-semibold px-8 py-3 hover:bg-gray-700"
+      className="rounded-lg bg-gray-800 w-50 text-white font-semibold px-8 py-3 hover:bg-gray-700"
     >
       Cancel
     </Button>
